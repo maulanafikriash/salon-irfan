a029d3493ae47931b3c2fe96233ff8d52de60315
maulanafikriash <maulanx835@gmail.com>
2024-01-29 07:07:45 +0700

    feat: create footer component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,7 @@
-/* eslint-disable jsx-a11y/label-has-associated-control */
 /* eslint-disable jsx-a11y/no-noninteractive-tabindex */
+/* eslint-disable react/jsx-one-expression-per-line */
+/* eslint-disable jsx-a11y/label-has-associated-control */
+
 import React, { useState, useEffect } from 'react';
 import { Link, useLocation } from 'react-router-dom';
 
@@ -21,10 +23,8 @@ export default function Navbar() {
         <div className="header d-lg-flex justify-content-between align-items-center">
           <div className="header-agile">
             <h1 tabIndex="0" className="brand">
-              <img src="assets/images/logo-salon-new.png" alt="logo salon irfan" className="logo-brand" />
-              <span>
-                Salon Irfan
-              </span>
+              <img src="assets/images/imagenya" alt="logo salon irfan" className="logo-brand" />
+              Salon Irfan
             </h1>
           </div>
           <div>
@@ -34,7 +34,7 @@ export default function Navbar() {
               </label>
               <input type="checkbox" id="drop" />
               <ul className="menu">
-                <li className={`m-2 ${activeLink === '/' ? 'active' : ''}`}>
+                <li className={`m-2 top-content ${activeLink === '/' ? 'active' : ''}`}>
                   <Link
                     className={`text-decoration-none p-2 ${activeLink === '/' ? 'active-link' : ''}`}
                     to="/"
@@ -43,40 +43,31 @@ export default function Navbar() {
                     Home
                   </Link>
                 </li>
-                <li className={`m-2 ${activeLink === '/about' ? 'active' : ''}`}>
+                <li className={`m-2 top-content ${activeLink === '/about' ? 'active' : ''}`}>
                   <Link
                     className={`text-decoration-none p-2 ${activeLink === '/about' ? 'active-link' : ''}`}
                     to="/about"
                     onClick={() => handleLinkClick('/about')}
                   >
-                    About
+                    Tentang
                   </Link>
                 </li>
-                <li className={`m-2 ${activeLink === '/services' ? 'active' : ''}`}>
+                <li className={`m-2 top-content ${activeLink === '/services' ? 'active' : ''}`}>
                   <Link
                     className={`text-decoration-none p-2 ${activeLink === '/services' ? 'active-link' : ''}`}
                     to="/services"
                     onClick={() => handleLinkClick('/services')}
                   >
-                    Services
+                    Layanan
                   </Link>
                 </li>
-                <li className={`m-2 ${activeLink === '/gallery' ? 'active' : ''}`}>
+                <li className={`m-2 top-content ${activeLink === '/gallery' ? 'active' : ''}`}>
                   <Link
                     className={`text-decoration-none p-2 ${activeLink === '/gallery' ? 'active-link' : ''}`}
                     to="/gallery"
                     onClick={() => handleLinkClick('/gallery')}
                   >
-                    Gallery
-                  </Link>
-                </li>
-                <li className={`m-2 ${activeLink === '/contact' ? 'active' : ''}`}>
-                  <Link
-                    className={`text-decoration-none p-2 ${activeLink === '/contact' ? 'active-link' : ''}`}
-                    to="/contact"
-                    onClick={() => handleLinkClick('/contact')}
-                  >
-                    Contact Us
+                    Galeri
                   </Link>
                 </li>
               </ul>
