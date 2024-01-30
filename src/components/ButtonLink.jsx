58d2aa9cbad5c578f8869b3652afb136517c0df1
maulanafikriash <maulanx835@gmail.com>
2024-01-30 23:52:11 +0700

    feat: add button link component

---
+++ src/components/ButtonLink.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+export default function ButtonLink() {
+  return (
+    <div className="mt-3 mb-3">
+      <Link to="/layanan" className="btn btn-dark">
+        <span>
+          Selengkapnya...
+        </span>
+      </Link>
+    </div>
+  );
+}
