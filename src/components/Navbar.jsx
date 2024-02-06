fc493e3f649f477382be3b4ce96f301cf35321f7
maulanafikriash <maulanx835@gmail.com>
2024-02-06 09:26:32 +0700

    fix: components

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,17 @@
+/* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
 /* eslint-disable jsx-a11y/no-noninteractive-tabindex */
-/* eslint-disable react/jsx-one-expression-per-line */
+/* eslint-disable jsx-a11y/click-events-have-key-events */
 /* eslint-disable jsx-a11y/label-has-associated-control */
 
 import React, { useState, useEffect } from 'react';
 import { Link, useLocation } from 'react-router-dom';
 import { FaBars } from 'react-icons/fa6';
+import { FaTimes } from 'react-icons/fa';
 
 export default function Navbar() {
   const location = useLocation();
   const [activeLink, setActiveLink] = useState(location.pathname);
+  const [isToggleActive, setToggleActive] = useState(false);
 
   useEffect(() => {
     setActiveLink(location.pathname);
@@ -18,13 +21,19 @@ export default function Navbar() {
 
   const handleLinkClick = (path) => {
     setActiveLink(path);
+    setToggleActive(false);
   };
+
+  const handleToggleClick = () => {
+    setToggleActive(!isToggleActive);
+  };
+
   return (
-    <header id="home">
+    <header id="home" className="fixed-top">
       <div className="container">
         <div className="header d-lg-flex justify-content-between align-items-center">
           <div className="header-agile">
-            <h1 tabIndex="0" className="brand">
+            <h1 tabIndex={0} className="brand">
               <img
                 src="assets/images/logo-salon-nav.webp"
                 alt="logo salon irfan"
@@ -35,64 +44,49 @@ export default function Navbar() {
           </div>
           <div>
             <nav>
-              <label htmlFor="drop" className="toggle mt-lg-0 mt-1">
-                <FaBars />
+              <label htmlFor="drop" className="toggle mt-lg-0 mt-1" onClick={handleToggleClick}>
+                {isToggleActive ? <FaTimes /> : <FaBars />}
               </label>
-              <input type="checkbox" id="drop" />
-              <ul className="menu">
-                <li
-                  className={`m-2 top-content ${isHomeActive ? 'active' : ''}`}
-                >
+              <ul className={`menu ${isToggleActive ? 'show' : ''}`}>
+                <li className={`m-2 top-content ${isHomeActive ? 'active' : ''}`}>
                   <Link
-                    className={`text-decoration-none p-2 ${
-                      isHomeActive ? 'active-link' : ''
-                    }`}
+                    className={`text-decoration-none p-2 ${isHomeActive ? 'active-link' : ''}`}
                     to="/"
-                    onClick={() => setActiveLink('/')}
+                    onClick={() => handleLinkClick('/')}
                   >
                     Beranda
                   </Link>
                 </li>
-
-                <li
-                  className={`m-2 top-content ${
-                    activeLink === '/tentang' ? 'active' : ''
-                  }`}
-                >
+                <li className={`m-2 top-content ${activeLink === '/tentang' ? 'active' : ''}`}>
                   <Link
-                    className={`text-decoration-none p-2 ${
-                      activeLink === '/tentang' ? 'active-link' : ''
-                    }`}
+                    className={`text-decoration-none p-2 ${activeLink === '/tentang' ? 'active-link' : ''}`}
                     to="/tentang"
                     onClick={() => handleLinkClick('/tentang')}
                   >
                     Tentang
                   </Link>
                 </li>
-                <li
-                  className={`m-2 top-content ${
-                    activeLink === '/layanan' ? 'active' : ''
-                  }`}
-                >
+                <li className={`m-2 top-content ${activeLink === '/layanan' ? 'active' : ''}`}>
                   <Link
-                    className={`text-decoration-none p-2 ${
-                      activeLink === '/layanan' ? 'active-link' : ''
-                    }`}
+                    className={`text-decoration-none p-2 ${activeLink === '/layanan' ? 'active-link' : ''}`}
                     to="/layanan"
                     onClick={() => handleLinkClick('/layanan')}
                   >
                     Layanan
                   </Link>
                 </li>
-                <li
-                  className={`m-2 top-content ${
-                    activeLink === '/galeri' ? 'active' : ''
-                  }`}
-                >
+                <li className={`m-2 top-content ${activeLink === '/pemesanan' ? 'active' : ''}`}>
                   <Link
-                    className={`text-decoration-none p-2 ${
-                      activeLink === '/galeri' ? 'active-link' : ''
-                    }`}
+                    className={`text-decoration-none p-2 ${activeLink === '/pemesanan' ? 'active-link' : ''}`}
+                    to="/pemesanan"
+                    onClick={() => handleLinkClick('/pemesanan')}
+                  >
+                    Pemesanan
+                  </Link>
+                </li>
+                <li className={`m-2 top-content ${activeLink === '/galeri' ? 'active' : ''}`}>
+                  <Link
+                    className={`text-decoration-none p-2 ${activeLink === '/galeri' ? 'active-link' : ''}`}
                     to="/galeri"
                     onClick={() => handleLinkClick('/galeri')}
                   >
